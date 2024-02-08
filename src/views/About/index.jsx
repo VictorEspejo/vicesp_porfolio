cece90995f8c62c2af7aee97c4681e026766d965
VictorEspejo <33323490+VictorEspejo@users.noreply.github.com>
2024-02-08 17:48:57 +0000

    feat: change image

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -1,6 +1,6 @@
 import WaveBottom from "../../components/WaveBottom";
 import WaveTop from "../../components/WaveTop";
-import VictorImage from "../../assets/images/victor.jpg";
+import VictorImage from "../../assets/images/victor.png";
 
 export default function About() {
   return (
@@ -10,17 +10,17 @@ export default function About() {
         <h1 className="w-full text-6xl font-bold text-center py-16">
           About Me
         </h1>
-        <div className="flex flex-col gap-16 lg:flex-row wrap justify-center items-center py-16">
-          <div className="flex justify-center w-full">
+        <div className="grid md:grid-cols-2 gap-6 md:gap-0 lg:px-32">
+          <div className="flex justify-center w-full grid-span-2 md:grid-span-1">
             <img
               src={VictorImage}
               alt="Victor Dev"
-              className="w-72 h-72 md:w-96 md:h-96 object-cover rounded-full"
+              className="w-72 h-72 md:w-96 md:h-96 object-cover rounded-full bg-white border-4 border-sky-600 dark:border-sky-800 shadow-2xl"
             />
           </div>
           <div className="flex flex-col justify-center items-center text-white px-10 lg:px-8">
-            <h1 className="text-4xl font-bold">Victor Espejo</h1>
-            <p className="text-xl mt-4 leading-8 lg:w-1/2">
+            <h1 className="text-4xl font-bold w-full text-align-start">Victor Espejo</h1>
+            <p className="text-xl mt-4 leading-8 lg:pr-32 text-align-start">
               I'm Victor Espejo, an experienced front-end developer with over 5
               years in the banking sector. Skilled in multiple frameworks, my
               passions extend to technology, cinema, and video games.
--- src/views/Welcome/index.jsx
+++ src/views/Welcome/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import CV from "@assets/documents/Victor_Espejo_CV.pdf";
+import VictorLogo from "../../../public/victorlogo.png"
 
 export default function Welcome() {
   return (
     <section className="w-full text-black dark:text-white">
       <header className="w-full px-16 py-4">
-        <h1>Victor Dev</h1>
+        <img src={VictorLogo} alt="Victor Espejo" className="w-16 h-16" />
       </header>
       <section className="w-full flex flex-col items-center text-center lg:text-left py-32 lg:py-56">
         <h1 className="text-7xl font-bold">
