02e2153f0592e7f825ca267c9af814927edb4c32
VictorEspejo <33323490+VictorEspejo@users.noreply.github.com>
2024-02-23 15:21:30 +0000

    style: Remove unnecessary code related to animation controls in Footer component and update Welcome component styling to include padding in section element

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,27 +6,11 @@ import { RxHalf1 } from "react-icons/rx";
 import { RxEnvelopeClosed } from "react-icons/rx";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
-import { motion, useAnimationControls } from "framer-motion";
+import { motion } from "framer-motion";
 import { signal } from "@preact/signals-react";
 
 export default function Footer() {
   const darkMode = signal(false);
-
-  const animation = useAnimationControls();
-
-  async function sequence() {
-    await animation.start({
-      y: "-80vh",
-      scale: 0.25,
-      opacity: 0,
-    });
-    await animation.start({
-      y: "0vh",
-      opacity: 0.25,
-      transition: { duration: 0.5 },
-    });
-    animation.start({ scale: 1, opacity: 0.5 });
-  }
 
   const handleDarkMode = () => {
     darkMode.value = !darkMode.value;
@@ -36,11 +20,9 @@ export default function Footer() {
 
   return (
     <motion.nav
-      initial={{ y: "-90vh", scale: 0.25 }}
-      animate={animation}
-      whileInView={sequence}
-      viewport={{ once: true }}
-      transition={{ duration: 2, type: "spring", stiffness: 120 }}
+      initial={{ scale: 0.1 }}
+      animate={{ scale: 1 }}
+      transition={{ duration: 2.5, type: "spring", stiffness: 150 }}
       className="w-full flex justify-center opacity-50 mx-auto"
     >
       <ul className="bg-black text-white py-4 px-24 rounded-full flex justify-center items-center gap-6">
